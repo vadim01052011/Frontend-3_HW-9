f8416f7cfdc76221352f2500bcab47f9345c55f3
vadim01052011 <166243362+vadim01052011@users.noreply.github.com>
2025-05-09 20:44:06 +0300

    HW-9

---
+++ js/arrow.js
@@ -0,0 +1,84 @@
+// function declaration
+
+// function countFriends(friends) {
+// console.log(friends);
+// const result = friends.length;
+// return result
+// }
+
+
+// function expression
+// const countFriends = function(friends) {
+// console.log(friends);
+// const result = friends.length;
+// return result
+// }
+
+
+
+// function expression стрілкова функція явне повернення
+// const countFriends = (friends) => {
+// const result = friends.length;
+// return result
+// }
+
+// function expression стрілкова функція НЕ ЯВНЕ повернення
+// const countFriends = (friends) => friends.length
+
+
+
+
+// const userGame = ["oleksandr","nazar" ,"ivanD", "ivanM", "mikita", "vadim", "taras", "miroslav", "dima"];
+// console.log(countFriends(userGame));
+
+
+// const add = function(...args) {  
+//     // const result = Array.from(arguments)
+//     console.log(args);
+// }
+
+// const add = (...args) => {
+//     console.log(args);
+// }
+
+// console.log(add(1, 2, 5, 10, 20));
+
+
+// #1
+// const isEven = (num) => {
+//   return num % 2 === 0;
+// }
+
+// #2
+// const isEven = (num) => num % 2 === 0;
+
+
+// console.log(isEven(6));
+
+
+
+// #1
+// const stringLenght = (str) => {
+//     return str.length;
+// }
+
+// #2
+// const stringLenght = (str) => str.length;
+
+// console.log(stringLenght("I love JS"));
+
+
+
+// Напишіть функцію, яка приймає на вхід масив чисел і повертає новий масив, в якому кожний елемент є квадратом відповідного елементу вхідного масиву.
+
+// const squereArray = (array) => {
+//     const result = [];
+// for(let i = 0; i < massive.lenght; i++) {
+//    result.push(array[i] * array[i])
+
+// }
+// return result
+// }
+// const massive = [7, 12, 777]
+// console.log(massive);
+
